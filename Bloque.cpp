9029f9dbc8088f6caaab29f9baf9ca7cd731b99a
Martin reynolds <martin_reynolds_122@hotmail.com>
2024-06-01 06:56:27 -0300

    cambios en la clase bloque

--- Bloque.cpp
+++ Bloque.cpp
@@ -9,3 +9,23 @@ Bloque::Bloque(String archivo, Vector2i a, Vector2f b, int e) {
 	tx_bloque->loadFromFile(archivo);
 	sp_bloque->setTexture(*tx_bloque);
 }
+
+Sprite get_sprite() {
+    
+}
+void Bloque::set_sprite(Texture newTexture) {
+	sp_bloque->setTexture(newTexture);
+}
+void Bloque::set_position(float x,float y) {
+	sp_bloque->setPosition(x, y);
+}
+Vector2f Bloque::get_position() {
+	Vector2f pos;
+	pos.x = sp_bloque->getPosition().x;
+	pos.y = sp_bloque->getPosition().y;
+	return pos;
+}
+
+
+
+}
--- Bloque.h
+++ Bloque.h
@@ -11,4 +11,7 @@ public:
 	Bloque();
 	Bloque(String, Vector2i, Vector2f, int);
 	Sprite get_sprite();
+	void set_sprite(Texture);
+	void set_position(float,float);
+	Vector2f get_position();
 };
